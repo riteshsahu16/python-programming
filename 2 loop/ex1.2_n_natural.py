4b5fde1a8c9e9c203644701bf74c904d6226204f
riteshsahu16 <riteshsahu0116@gmail.com>
2021-04-24 22:10:25 +0530

    solved loop problem

---
+++ 2 loop/ex1.2_n_natural.py
@@ -0,0 +1,4 @@
+#Q2. Write a program to print N natural no.
+n = int(input())
+for i in range(1,n+1):
+    print(i, end=" ")
---
+++ 2 loop/ex1.3_first_n_even.py
@@ -0,0 +1,8 @@
+#Q3. Write a program to print first N even no.
+n = int(input())
+count = 0
+even = 0
+while count < n:
+    print(even, end=" ")
+    count += 1
+    even += 2
